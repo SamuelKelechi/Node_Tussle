bb1044dc1ef404356fbec9c0cb88d00beb400330
SamuelKelechi <kcfreshie@ymail.com>
2022-12-09 13:43:03 +0100

    Working with File System

--- myfirst.js
+++ myfirst.js
@@ -1,5 +1,23 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
+var fs = require('fs');
+
+
+http.createServer(function (req, res) {
+  fs.readFile('Sam.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+
+  fs.appendFile('Sam.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+}).listen(8070);
+
+
+
 
 // http.createServer(function (req, res) {
 //   res.writeHead(200, {'Content-Type': 'text/html'});
@@ -9,9 +27,3 @@ var dt = require('./myfirstmodule');
 // }).listen(8080);
 
 // var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
-}).listen(8080);
